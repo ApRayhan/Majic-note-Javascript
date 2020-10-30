49e67ced05612185c29f5c9ddc3481b00ce6be71
Rayhan <Rayhandimla@gmail.com>
2020-10-30 17:03:53 +0600

    first commit

---
+++ App.js
@@ -0,0 +1,91 @@
+console.log('its work');
+getNotes();
+
+const addNoteBtn = document.getElementById('addNoteBtn');
+
+// Add A Note
+addNoteBtn.addEventListener('click', function(e){
+  e.preventDefault();
+  let noteTxt = document.getElementById('noteTxt');
+  let notes = localStorage.getItem('notes');
+  if (notes == null) {
+    noteObject = [];
+  }
+  else {
+    noteObject = JSON.parse(notes);
+  }
+  noteObject.push(noteTxt.value);
+  localStorage.setItem('notes', JSON.stringify(noteObject));
+  noteTxt.value = '';
+  getNotes();
+})
+
+// Get All Notes
+function getNotes(){
+  let notes = localStorage.getItem('notes');
+
+  if (notes == null) {
+    noteObject = [];
+  }
+  else {
+    noteObject = JSON.parse(notes);
+  }
+  let html = "";
+
+  noteObject.forEach(function(element, index){
+    // console.log(element, index);
+    html += `
+              <div class="col-md-4 mt-3">
+                <div class="card">
+                  <div class="card-body">
+                    <h5 class="card-title">Card title ${index +1}</h5>
+                    <p class="card-text">${element}</p>
+                    <button id="${index}" onclick="deleteFn(this.id)" class="btn btn-primary">Delete Note</button>
+                  </div>
+                </div>
+              </div>
+            `;
+  })
+
+  let allNotes = document.getElementById('notes');
+  if (notes.length != 0){
+    allNotes.innerHTML = html;
+  }
+}
+
+// Delete Note
+
+function deleteFn(index){
+  
+  let notes = localStorage.getItem('notes');
+  console.log(JSON.parse(notes));
+  if (notes == null){
+    noteObject = [];
+  }
+  else {
+    noteObject = JSON.parse(notes);
+  }
+
+  noteObject.splice(index, 1);
+  localStorage.setItem('notes', JSON.stringify(noteObject));
+  getNotes();
+}
+
+// Search Function
+
+let searchTxt = document.getElementById('serachTxt');
+searchTxt.addEventListener('input', function(){
+  let searchValue = searchTxt.value.toLowerCase();
+  let note = document.getElementsByClassName('card');
+
+  Array.from(note).forEach(function(element){
+    let cartTxt = element.getElementsByTagName('p')[0].innerText.toLowerCase();
+
+    if (cartTxt.includes(searchValue)){
+      element.style.display = 'block';
+    }
+    else {
+      element.style.display = 'none';
+    }
+  })
+})
---
+++ README.md
@@ -0,0 +1,6 @@
+# Majic-note-Javascript
+
+Its A Note App . You Can Add A Note , And Delete Any Note .
+You Can Search A Note .
+
+Technology : JavaScript, Html, Bootstrap
